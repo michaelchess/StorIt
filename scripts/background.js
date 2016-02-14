da4d7780e8aa895a032823f8dabd87169a564aa0
Jared Wright <jawerty@172-16-120-168.DYNAPOOL.NYU.EDU>
2016-02-13 20:31:08 -0500

    add background script page

---
+++ scripts/background.js
@@ -0,0 +1,34 @@
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.getHTML) {
+    	var HTMLString = request.getHTML;
+    	var doctype = document.implementation.createDocumentType( 'html', '', '');
+		var dom = document.implementation.createDocument('', 'html', doctype);
+		dom.documentElement.innerHTML = HTMLString;
+
+		var query = dom.title;
+
+		$.ajax({
+            type: "GET", //or GET
+            url: "http://www.jstor.org/action/doBasicSearch?Query="+query+"&acc=on&wc=on&fc=off&group=none",
+            success: function(data){
+            	console.log(data);
+			},
+			error: function(jxhr){
+			   console.log(jxhr.responseText);
+			}
+        });
+    }
+});
+
+$.ajax({
+    type: "GET", //or GET
+    url: "http://www.jstor.org/action/doBasicSearch?Query="+query+"&acc=on&wc=on&fc=off&group=none",
+    success: function(data){
+    	console.log(data);
+	},
+	error: function(jxhr){
+		console.log(jxhr.responseText);
+	}
+});
+
+console.log("popup")
