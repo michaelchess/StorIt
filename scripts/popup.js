c82d5fc7bf700932c7476b2354bab14beed2d0df
Michael Chess <michael.chess14@trinityschoolnyc.org>
2016-02-14 16:46:22 -0500

    added buttons for selecting source

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,22 @@
 window.onload = function(){
 
-	document.getElementById("SendDOM").onclick = function () {
+	function onclick(source){
+		document.getElementById("results").innerHTML = "";
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  			chrome.tabs.sendMessage(tabs[0].id, {salutation: true}, function(response) {
+  			chrome.tabs.sendMessage(tabs[0].id, {salutation: true, source: source}, function(response) {
   				console.log(response);
   			});
 		});
 	}
+
+	document.getElementById("jstor").onclick = function () {
+		onclick("jstor");
+	}
+	
+	document.getElementById("googlescholar").onclick = function () {
+		onclick("googlescholar");
+	}
+
 	chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 		if (request.results) {
 			for (var i = 0; i < request.results.length; i++) {
