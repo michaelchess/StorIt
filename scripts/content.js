e0c3f45b78d26509cb66c37168505f5f2c970202
Michael Chess <michael.chess14@trinityschoolnyc.org>
2016-02-13 20:31:29 -0500

    basic content to send dom document

---
+++ scripts/content.js
@@ -0,0 +1,6 @@
+chrome.runtime.sendMessage({DOM: document.documentElement.innerHTML}, function(response) {
+	if(response.success == true){
+		console.log("Success");
+	}
+});
+//document.documentElement.innerHTML();
